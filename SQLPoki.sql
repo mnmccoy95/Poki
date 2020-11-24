328257175b6666aff8f8902dfd1c436f6a077c95
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-24 15:52:43 -0600

    first 3 queries complete

---
+++ SQLPoki.sql
@@ -0,0 +1,13 @@
+﻿--SELECT DISTINCT Name
+--FROM Grade;
+
+--SELECT DISTINCT Name
+--FROM Emotion;
+
+--SELECT COUNT(Poem.id) AS PoemCount
+--FROM Poem;
+
+SELECT Name
+FROM Author
+WHERE Id > 77
+ORDER BY Name;
